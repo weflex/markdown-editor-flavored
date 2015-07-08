05b7a4885abb7bffe47f07468f97f486b9535b3b
Yazhong Liu <yorkiefixer@gmail.com>
2015-07-09 00:41:41 +0800

    update gh-pages

---
+++ ghpages.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+REMOTE=`git remote -v | grep origin | grep push | awk '{split($0,a," "); print a[2]}'`
+cd ./
+
+git init
+git add .
+git commit -a -m 'update gh-pages'
+git branch 'gh-pages'
+git checkout 'gh-pages'
+git push $REMOTE 'gh-pages' --force
